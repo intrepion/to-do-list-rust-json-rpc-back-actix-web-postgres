20818b8e60a8abdf72c4f41cb3c3ba5ee96ccde5
Oliver Forral <intrepion@gmail.com>
2022-03-01 15:59:16 -0700

    green

--- to-do-list-json-rpc-back-actix-web-postgres/src/json_rpc.rs
+++ to-do-list-json-rpc-back-actix-web-postgres/src/json_rpc.rs
@@ -2,11 +2,15 @@
 mod tests {
     mod json_rpc_error {
         mod default_should {
+            use super::super::super::JsonRpcError;
+            use serde_json;
+
             #[test]
             fn return_new_error() {
-                let _expected =
-                    r#"{"json_rpc":{"code":1,"message":"This is the default error message."}}"#;
-                let actual = JsonRpcError::default().to_string();
+                let expected = r#"{"code":1,"message":"This is the default error message."}"#;
+                let actual = serde_json::to_string(&JsonRpcError::default()).unwrap();
+
+                assert_eq!(expected, actual);
             }
         }
     }
@@ -16,9 +20,20 @@ use serde::{Deserialize, Serialize};
 
 #[derive(Deserialize, Serialize)]
 pub struct JsonRpcError {
-    code: String,
+    code: i64,
+    #[serde(skip_serializing_if = "Option::is_none")]
     data: Option<serde_json::Value>,
     message: String,
+}
+
+impl JsonRpcError {
+    pub fn default() -> Self {
+        JsonRpcError {
+            code: 1,
+            data: None,
+            message: "This is the default error message.".to_owned(),
+        }
+    }
 }
 
 #[derive(Deserialize, Serialize)]
