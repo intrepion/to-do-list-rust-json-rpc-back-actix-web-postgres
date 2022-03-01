f6a36407bdd30aae82ab990b83f0a2481fd89678
Oliver Forral <intrepion@gmail.com>
2022-02-28 22:29:09 -0700

    red

--- to-do-list-json-rpc-back-actix-web-postgres/src/lib.rs
+++ to-do-list-json-rpc-back-actix-web-postgres/src/lib.rs
@@ -1,8 +1,9 @@
 #[cfg(test)]
 mod tests {
-    #[actix_web::test]
-    async fn it_works() {
-        let result = 2 + 2;
-        assert_eq!(result, 4);
+    mod health_check_should {
+        #[actix_web::test]
+        async fn return_ok() {
+            let app = test::init_service(App::new().wrap(middleware::Logger::default()).service(api)).await();
+        }
     }
 }
