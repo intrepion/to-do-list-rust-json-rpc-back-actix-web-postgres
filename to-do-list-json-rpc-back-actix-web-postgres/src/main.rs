d27efa45367fd5825e2f6fbfb6a28ae690f6e10a
Oliver Forral <intrepion@gmail.com>
2022-03-01 00:14:20 -0700

    blue

--- to-do-list-json-rpc-back-actix-web-postgres/src/main.rs
+++ to-do-list-json-rpc-back-actix-web-postgres/src/main.rs
@@ -1,3 +1,10 @@
-fn main() {
-    println!("Hello, world!");
+use actix_web::{App, HttpServer};
+use to_do_list_json_rpc_back_actix_web_postgres::api;
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    HttpServer::new(|| App::new().service(api))
+        .bind(("127.0.0.1", 8080))?
+        .run()
+        .await
 }
