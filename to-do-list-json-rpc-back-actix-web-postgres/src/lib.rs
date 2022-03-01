f5642e65bdbe4e5c2bb0ed9f8239863b477f6bb1
Oliver Forral <intrepion@gmail.com>
2022-02-28 23:59:10 -0700

    green

--- to-do-list-json-rpc-back-actix-web-postgres/src/lib.rs
+++ to-do-list-json-rpc-back-actix-web-postgres/src/lib.rs
@@ -1,43 +1,65 @@
 #[cfg(test)]
 mod tests {
     mod health_check_should {
-        use super::super::{api, JsonRpc2Point0Request, ToDoListMethod};
+        use super::super::{api, JsonRpcRequest, JsonRpcResponse, ToDoListMethod};
         use actix_web::{middleware, test, App};
 
         #[actix_web::test]
         async fn return_ok() {
-            let app =
+            let mut app =
                 test::init_service(App::new().wrap(middleware::Logger::default()).service(api))
                     .await;
             let req = test::TestRequest::post()
                 .uri("/api")
-                .set_json(JsonRpc2Point0Request {
+                .set_json(JsonRpcRequest {
                     id: Some("1".to_owned()),
+                    json_rpc: "2.0".to_owned(),
                     method: ToDoListMethod::HealthCheck,
                     params: None,
                 })
                 .to_request();
-            let _resp: JsonRpc2Point0Response = test::call_and_read_body_json(&mut app, req).await;
+            let _resp: JsonRpcResponse = test::call_and_read_body_json(&mut app, req).await;
         }
     }
 }
 
-use actix_web::{post, Error, HttpResponse};
-use serde::Serialize;
+use actix_web::{post, web, Error, HttpResponse};
+use serde::{Deserialize, Serialize};
 
-#[derive(Serialize)]
-pub struct JsonRpc2Point0Request {
-    pub id: Option<String>,
-    pub method: ToDoListMethod,
-    pub params: Option<String>,
+#[derive(Deserialize, Serialize)]
+pub struct JsonRpcError {
+    pub code: String,
+    pub data: Option<serde_json::Value>,
+    pub message: String,
 }
 
-#[derive(Serialize)]
+#[derive(Deserialize, Serialize)]
+pub struct JsonRpcRequest {
+    pub id: Option<String>,
+    pub json_rpc: String,
+    pub method: ToDoListMethod,
+    pub params: Option<serde_json::Value>,
+}
+
+#[derive(Deserialize, Serialize)]
+pub struct JsonRpcResponse {
+    pub error: Option<JsonRpcError>,
+    pub id: Option<String>,
+    pub json_rpc: String,
+    pub result: Option<serde_json::Value>,
+}
+
+#[derive(Deserialize, Serialize)]
 pub enum ToDoListMethod {
     HealthCheck,
 }
 
 #[post("/api")]
-pub async fn api() -> Result<HttpResponse, Error> {
-    Ok(HttpResponse::Ok().json("{}"))
+pub async fn api(req: web::Json<JsonRpcRequest>) -> Result<HttpResponse, Error> {
+    Ok(HttpResponse::Ok().json(JsonRpcResponse {
+        error: None,
+        id: req.id.clone(),
+        json_rpc: "2.0".to_owned(),
+        result: None,
+    }))
 }
