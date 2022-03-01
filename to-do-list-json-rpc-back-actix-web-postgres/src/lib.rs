c9850fed0fd95bc22cf53af7222a30ae5d332e5b
Oliver Forral <intrepion@gmail.com>
2022-02-28 22:33:57 -0700

    green

--- to-do-list-json-rpc-back-actix-web-postgres/src/lib.rs
+++ to-do-list-json-rpc-back-actix-web-postgres/src/lib.rs
@@ -1,9 +1,21 @@
 #[cfg(test)]
 mod tests {
     mod health_check_should {
+        use super::super::api;
+        use actix_web::{middleware, test, App};
+
         #[actix_web::test]
         async fn return_ok() {
-            let app = test::init_service(App::new().wrap(middleware::Logger::default()).service(api)).await();
+            let _app =
+                test::init_service(App::new().wrap(middleware::Logger::default()).service(api))
+                    .await;
         }
     }
+}
+
+use actix_web::{post, Error, HttpResponse};
+
+#[post("/api")]
+pub async fn api() -> Result<HttpResponse, Error> {
+    Ok(HttpResponse::Ok().json("{}"))
 }
