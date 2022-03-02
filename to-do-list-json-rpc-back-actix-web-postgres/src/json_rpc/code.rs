14e8479405d94d73d922a2fc570662b30c2628fe
Oliver Forral <intrepion@gmail.com>
2022-03-01 23:24:35 -0700

    green

--- to-do-list-json-rpc-back-actix-web-postgres/src/json_rpc/code.rs
+++ to-do-list-json-rpc-back-actix-web-postgres/src/json_rpc/code.rs
@@ -2,11 +2,24 @@
 mod tests {
     mod test_code {
         mod default_should {
+            use super::super::super::Code;
+
             #[test]
             fn return_new_code() {
-                let expected = r#"{0}"#;
-                let actual = serde_json::to_string(&Code::default()).unwrap();
+                let _expected = r#"{0}"#;
+                let _actual = serde_json::to_string(&Code::default()).unwrap();
             }
         }
+    }
+}
+
+use serde::{Deserialize, Serialize};
+
+#[derive(Deserialize, Serialize)]
+pub struct Code(i64);
+
+impl Code {
+    pub fn default() -> Self {
+        Code(0)
     }
 }
